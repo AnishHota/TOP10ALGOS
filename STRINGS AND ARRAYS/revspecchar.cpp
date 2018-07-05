fe3e740b47a32e93bee023f053211d16beb9316d
anishhota <anishhota92@gmail.com>
2018-07-05 08:33:53 +0530

    Adds Strings and Arrays section

---
+++ STRINGS AND ARRAYS/revspecchar.cpp
@@ -0,0 +1,35 @@
+// REVERSE AN ARRAY WITHOUT AFFECTING SPECIAL CHARACTERS
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+  string s;
+  cin>>s;
+  int n = s.length();
+  int ans[n];
+  int i=0;
+  int j = n-1;
+  while(i<j)
+  {
+    if(isalpha(s[i]) && isalpha(s[j]))
+    {
+      swap(s[i],s[j]);
+      i++;
+      j--;
+    }
+    else if(!isalpha(s[i]))
+    {
+      i++;
+    }
+    else if(!isalpha(s[j]))
+    {
+      j--;
+    }
+  }
+  for(i=0;i<s.length();i++)
+  {
+    cout<<s[i];
+  }
+  return 0;
+}
