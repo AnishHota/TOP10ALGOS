a5b252b65fbc3381e8522e4343071c7499a46796
anishhota <anishhota92@gmail.com>
2019-06-07 23:22:10 +0530

    Updates Longest Common Subsequence

--- DYNAMIC PROGRAMMING/Longest Common Subsequence.cpp
+++ DYNAMIC PROGRAMMING/Longest Common Subsequence.cpp
@@ -9,20 +9,41 @@ int lcs(int m,int n)
   {
     for(j=0;j<=n;j++)
     {
-        if(i==0 || j==0)
-          arr[i][j]=0;
-        else if(x[i-1]==y[j-1])
-          arr[i][j] = 1+arr[i-1][j-1];
-        else
-          arr[i][j] = max(arr[i-1][j],arr[i][j-1]);
+      if(i==0 || j==0)
+        arr[i][j]=0;
+      else if(x[i-1]==y[j-1])
+        arr[i][j] = 1+arr[i-1][j-1];
+      else
+        arr[i][j] = max(arr[i-1][j],arr[i][j-1]);
     }
   }
+  i=m;
+  j=n;
+  string lc = "";
+  while(i>0 && j>0)
+  {
+    if(x[i-1]==y[j-1])
+    {
+      lc+=x[i-1];
+      i--;j--;
+    }
+    if(arr[i-1][j]>arr[i][j-1])
+      i--;
+    else
+      j--;
+  }
+  for(i=lc.length()-1;i>=0;i--)
+  {
+    cout<<lc[i]<<" ";
+  }
+  cout<<endl;
   return arr[m][n];
 }
 int main()
 {
   cin>>x>>y;
-  int i;
   int ans = lcs(x.length(),y.length());
   cout<<ans<<endl;
+  return 0;
 }
+
